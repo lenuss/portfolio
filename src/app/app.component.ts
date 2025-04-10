6d12e5c9cc7d03d0aa46485fadca4a8e749a4f4f
Lenka <lenka.bistakova@gmail.com>
2025-04-10 15:23:38 -0700

    basic layout setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['../demo-styling.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'angular-quickstart';
+  title = 'portfolio';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './sections/home/home.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { LayoutComponent } from './layout/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
